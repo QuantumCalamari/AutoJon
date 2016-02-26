11bbd1b8f75e56d4e292abf32d51e140b44b85a9
Jon Collins <jonacollins3@gmail.com>
2016-02-26 14:38:29 +0100

    start of the trawl is done

--- src/AutoJon/src/main.java
+++ src/AutoJon/src/main.java
@@ -6,6 +6,9 @@ import org.jsoup.helper.*;
 import org.jsoup.nodes.*;
 import org.jsoup.select.*;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 import AutoJon.src.trawl;
 
 public class main {
@@ -18,11 +21,6 @@ public class main {
 		
 		try {
 			
-		//	String[] urlStore = {"http://www.s1jobs.com/jobs/find?keywords_required=physics&onlyshowme=datesmart",
-		//			 "http://www.s1jobs.com/jobs/find?keywords_required=c%2B%2B&onlyshowme=datesmart",
-		//			"http://www.s1jobs.com/jobs/find?keywords_required=java&onlyshowme=datesmart",
-		//			"http://www.s1jobs.com/jobs/find?keywords_required=junior%20software&onlyshowme=datesmart"
-		//	};
 			ArrayList<String> urlStore = new ArrayList<String>();
 			ArrayList<String> jobStore = new ArrayList<String>();
 			
@@ -33,45 +31,73 @@ public class main {
 			
 			for (int i = 0; i < urlStore.size(); i++) {
 				
-				System.out.println();
-				System.out.println(urlStore.get(i));
-				System.out.println();
+				//System.out.println();
+				//System.out.println(urlStore.get(i));
+				//System.out.println();
+				
+				boolean add;
+				
+//				jobStore.add("Physics Teacher");
+//				jobStore.add("Senior Programmer");
 				
 				org.jsoup.nodes.Document doc = Jsoup.connect(urlStore.get(i)).get();
 				Elements links = doc.select("a[href]");
 				
 				String linkString = "";
-								
-				//System.out.println(trawl.someFunc());
 				
 				for (Element link : links) {
 		            	
 						linkString = link.attr("href");
 		               	String linkInnerH = link.html();
 		            	
-		            	
+		               	add = trawl.searchString(linkInnerH, linkString);
+		               	
+		               	if (add) {
+		               		jobStore.add(linkString);
+		               	}
+		               	
+		              // 	for (int j = 0; j < jobStore.size(); j++) {
+		               	
+		               //		System.out.println(linkInnerH);
+		               		           	
+		              // 	}
+		            	/*
 		            	if (linkInnerH.contains("Software")) {
 		            		System.out.println(linkInnerH);
 		            		System.out.println("www.s1jobs.com" + linkString);
 		            		jobStore.add("www.s1jobs.com" + linkString);
 		            		//System.out.println();
-		            	}
+		            	}*/
 		        	}
 				
 				System.out.println(jobStore.size());
 			}
-		
+			
+			//take job store and search for duplicates
+			
+			Set<String> set = new HashSet<String>(jobStore);
+
+			System.out.println(set.size());
+			System.out.println(jobStore.size());
+			
+			if (set.size() < jobStore.size()) {
+				jobStore.clear();
+				jobStore.addAll(set);
+			}
+			
+			for (int i = 0; i < jobStore.size(); i++) {
+				System.out.println("www.s1jobs.com" + jobStore.get(i));
+			}
+			
+			//now I need to take this data and search each url
+			
+			for (int i = 0; i < jobStore.size(); i++) {
+				trawl.pullJob(jobStore.get(i));
+			}
+					
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		//String title = doc.title();
 		
-		
-		
-		
-		
-
-	}
-
+	}	
 }
--- src/AutoJon/src/trawl.java
+++ src/AutoJon/src/trawl.java
@@ -1,9 +1,34 @@
 package AutoJon.src;
 
+import java.util.ArrayList;
+
 public class trawl {
 
-	public static String someFunc() {
-		return "dildos";
+	public static boolean searchString(String listing, String url) {
+		
+		boolean add = false;
+		
+		ArrayList<String> searchTerm = new ArrayList<String>();
+		
+		searchTerm.add("C++");
+		searchTerm.add("Junior Software");
+		searchTerm.add("Java");
+		
+		for (int i = 0; i < searchTerm.size(); i++) {
+			if (listing.contains(searchTerm.get(i))) {
+				//System.out.println(searchTerm.get(i));
+				//System.out.println(listing);
+				//System.out.println(url);
+				//System.out.println();
+				
+				add = true;
+				
+			}	
+		}
+		return add;
 	}
 	
+	public static void pullJob(String url) {
+		
+	}
 }
