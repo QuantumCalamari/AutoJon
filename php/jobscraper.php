7d75e65b42c4134109dac31e4128de8c3b9a8a4f
Jon Collins <jonacollins3@gmail.com>
2016-03-24 23:05:03 +0100

    added scraper

--- php/jobscraper.php
+++ php/jobscraper.php
@@ -1,7 +1,24 @@
+<h3>
+	AutoJon
+</h3>
+
 <?php
 
 $crawl = "http://www.s1jobs.com/jobs/find?keywords_required=c%2B%2B&onlyshowme=datesmart&order_by=date_posted";
-//$crawl = "http://bestspace.co";
+
+function scrapeHTML($url) {
+
+	$ch = curl_init();
+	$timeout = 5;
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+	$html = curl_exec($ch);
+	curl_close($ch);
+	
+	echo $url."<br />";
+	
+}
 
 function get_links($url) {
 	$input = @file_get_contents($url);
@@ -22,24 +39,11 @@ function get_links($url) {
 		if(substr($link, 0, 1) == ".") {
 			$link = substr($link, 1);
 		}
-		/*
-		if(substr($link,0,7) == "http://") {
-			$link = $link;
-		} else if(substr($link,0,8) == "https://") {
-			$link = $link;
-		} else if(substr($link,0,2) == "//") {
-			$link = substr($link, 2);
-		}  else if(substr($link,0,1) == "#") {
-			$link = $url;
-		}  else if(substr($link,0,7) == "mailto:") {
-			$link = "[".$link."]";
-		} else if (substr($link,0,5) == "/job/") {
-			$link = "http://www.s1jobs.com".$link;
-		}*/
 		
 		if (substr($link,0,5) == "/job/") {
 			if (strpos($link, "telecommunications")) {
 				$link = "http://www.s1jobs.com".$link;
+				scrapeHTML($link);
 			} else {
 				$link = $url;
 			}
@@ -47,7 +51,7 @@ function get_links($url) {
 			$link = $url;
 		}
 		
-				echo $link."<br />";	
+		//echo $link."<br />";	
 	}
 }
 
