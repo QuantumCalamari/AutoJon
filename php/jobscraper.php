3abc4d204a342180c645ac0448b96445a443e1eb
Jon Collins <jonacollins3@gmail.com>
2016-03-26 18:03:03 +0100

    ready to add button press

--- php/jobscraper.php
+++ php/jobscraper.php
@@ -4,11 +4,28 @@
 
 <?php
 
-$crawl = array("http://www.s1jobs.com/jobs/find?keywords_required=c%2B%2B&onlyshowme=datesmart&order_by=date_posted", "http://www.s1jobs.com/jobs/find?keywords_required=junior%20java&onlyshowme=datesmart&order_by=date_posted", "http://www.s1jobs.com/jobs/find?keywords_required=junior%20web%20developer&onlyshowme=datesmart&order_by=date_posted", "http://www.s1jobs.com/jobs/find?keywords_required=junior+developer&salary_lo=25000&salary_hi=55000&non_default_salary=0&facet_search=1&order_by=date_posted&cb=4473909", "http://www.s1jobs.com/jobs/find?keywords_required=physics&salary_lo=15000&salary_hi=65000&non_default_salary=0&facet_search=1&order_by=date_posted&cb=7235990");
+//$crawl = array("http://www.s1jobs.com/jobs/find?keywords_required=c%2B%2B&onlyshowme=datesmart&order_by=date_posted", "http://www.s1jobs.com/jobs/find?keywords_required=junior%20java&onlyshowme=datesmart&order_by=date_posted", "http://www.s1jobs.com/jobs/find?keywords_required=junior%20web%20developer&onlyshowme=datesmart&order_by=date_posted", "http://www.s1jobs.com/jobs/find?keywords_required=junior+developer&salary_lo=25000&salary_hi=55000&non_default_salary=0&facet_search=1&order_by=date_posted&cb=4473909", "http://www.s1jobs.com/jobs/find?keywords_required=physics&salary_lo=15000&salary_hi=65000&non_default_salary=0&facet_search=1&order_by=date_posted&cb=7235990");
+
+$crawl = array("http://www.s1jobs.com/jobs/find?keywords_required=physics&salary_lo=25000&salary_hi=55000&non_default_salary=0&facet_search=1&order_by=date_posted&cb=7210653");
 
 $search = array();
 
 function hitbutton($url) {
+	$ch = curl_init();
+	$timeout = 5;
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+	$html = curl_exec($ch);
+	curl_close($ch);
+	
+	echo "No Niall Bree";
+	
+	include("LIB_http.php");
+	$action = "http://www.s1jobs.com/apply/apply-submit.cgi";
+	$method= "POST";
+	$ref = "" ;
+	$response = http($target=$action, $ref, $method, $data_array="", EXCL_HEAD);
 	
 }
 
@@ -31,12 +48,16 @@ function scrapeHTML($url) {
 	$match = $matches[0];
 	echo $match;
 	
-	if ((strpos($match,"Senior")) || (strpos($match,"Teacher")) || (strpos($match,"Lead"))) {
-		//	echo "found a bad term";
-	} else {
-		//go to apply page and hit the apply button
-		apply($url);
-		echo "no bad terms found<br />";
+	if (strpos($html, "Niall Bree")) {
+		echo "Niall Bree <br />";
+	} else {	
+		if ((strpos($match,"Senior")) || (strpos($match,"Teacher")) || (strpos($match,"Lead"))) {
+			//	echo "found a bad term";
+		} else {
+			//go to apply page and hit the apply button
+			apply($url);
+			//echo "no bad terms found and no Niall Bree <br />";
+		}
 	}
 	
 	get_links($url);
@@ -64,7 +85,11 @@ function apply($url) {
 		if (substr($link,0,7) == "/apply/") {
 				$link = "http://www.s1jobs.com".$link;
 				echo "What we're looking for '".$link."<br />";
-				hitbutton($link);		
+			if (strpos($link, "jump")) {
+				echo "I don't know what to do with that <br />";
+			}	else {			
+				hitbutton($link);	
+			}
 		} else {
 			$link = $url;
 		}
